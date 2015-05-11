2e18e0bce41e2be4210e27229ea705efb1719dcf
Nick Markworth <marcolus17@users.noreply.github.com>
2015-05-11 16:14:55 -0500

    Initial Commit

---
+++ TaskIt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaskIt
+//
+//  Created by Nicholas Markworth on 5/11/15.
+//  Copyright (c) 2015 Nick Markworth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
